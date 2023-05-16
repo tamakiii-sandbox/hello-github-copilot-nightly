0146acce0aa5ef78ecb1bfd35704501da4feade5
tamakiii <tamakiii@users.noreply.github.com>
2023-05-16 13:39:02 +0900

    fix types

--- src/preload.ts
+++ src/preload.ts
@@ -1,9 +1,9 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
 window.addEventListener("DOMContentLoaded", () => {
-    const replaceText = (selector: string, text: string) => {
+    const replaceText = (selector: string, text: string | undefined) => {
       const element = document.getElementById(selector);
-      if (element) {
+      if (element && text) {
         element.innerText = text;
       }
     };
@@ -11,4 +11,4 @@ window.addEventListener("DOMContentLoaded", () => {
     for (const type of ["chrome", "node", "electron"]) {
       replaceText(`${type}-version`, process.versions[type as keyof NodeJS.ProcessVersions]);
     }
-  });
+});
